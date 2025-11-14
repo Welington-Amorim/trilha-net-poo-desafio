cb13b88b1db3da3321cf5ee3590d819940cc8127
Welington <welingtomwat@gmail.com>
2025-11-14 16:34:48 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,20 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // iPhone instala via 'App Store' (simulação)
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"[{Modelo} - iPhone] Instalando '{nomeApp}' pela App Store...");
+            // Simular processo
+            Console.WriteLine($"[{Modelo} - iPhone] '{nomeApp}' instalado com sucesso.");
+        }
     }
 }
