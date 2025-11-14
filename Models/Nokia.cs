833ceed8329956fb83f2b4397dd762b4d9dce8dd
Welington <welingtomwat@gmail.com>
2025-11-14 16:34:10 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,20 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // Nokia instala via 'Loja Android' (simulação)
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"[{Modelo} - Nokia] Instalando '{nomeApp}' pela Loja do Android...");
+            // Simular processo
+            Console.WriteLine($"[{Modelo} - Nokia] '{nomeApp}' instalado com sucesso.");
+        }
     }
 }
