91f5c4c10be23f760d6461daf2fe36282d5778f6
Welington <welingtomwat@gmail.com>
2025-11-14 16:35:32 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
-﻿using DesafioPOO.Models;
+using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Teste Nokia
+            Smartphone nokia = new Nokia("1199999-0000", "Nokia 3310", "111111111111111", 32);
+            Console.WriteLine("Smartphone Nokia:");
+            nokia.ExibirInfo();
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("Snake");
+
+            Console.WriteLine();
+
+            // Teste iPhone
+            Smartphone iphone = new Iphone("1198888-0000", "iPhone 11", "222222222222222", 128);
+            Console.WriteLine("Smartphone iPhone:");
+            iphone.ExibirInfo();
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine();
+            Console.WriteLine("Teste finalizado. Pressione ENTER para sair.");
+            Console.ReadLine();
+        }
+    }
+}
